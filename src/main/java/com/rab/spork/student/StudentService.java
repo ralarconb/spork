4814d6a1afe290f8adccac565f4888f97877bc60
ralarconb <ralarconbazan@gmail.com>
2021-04-18 14:38:11 -0500

    .

--- src/main/java/com/rab/spork/student/StudentService.java
+++ src/main/java/com/rab/spork/student/StudentService.java
@@ -1,14 +1,21 @@
 package com.rab.spork.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
 	public List<Student> getStudents() {
-		return List.of(new Student(1L, "Justina", "j.succi@gmail.com", LocalDate.of(2000, Month.APRIL, 6), 22));
+		return studentRepository.findAll();
 	}
 }
